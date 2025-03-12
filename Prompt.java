d1e245bc09445a983c2eed5e11037c4f88aaa4b0
fangg2000 <131591502+fangg2000@users.noreply.github.com>
2025-03-12 14:08:42 +0800

    Add files via upload
    update some moethods

---
+++ Prompt.java
@@ -0,0 +1,119 @@
+package com.fangg.bgm.api.bean.comfyui;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class Prompt {
+    private Map<String, PromptNode> nodes; 	// 存储所有节点
+    private Map<String, Integer> idMap;		// 缓存id顺序
+
+    public Prompt() {
+        nodes = new LinkedHashMap<>(); // 保持插入顺序
+    }
+
+	public Prompt(String str) {
+		nodes = new LinkedHashMap<>(); // 保持插入顺序
+		// 
+		JSONObject promptJson = JSONObject.parseObject(str);
+		PromptNode promptNode;
+		int maxNodeId = 0;
+		
+		for (Entry<String, Object> entry : promptJson.entrySet()) {
+			promptNode = JSONObject.parseObject(JSONObject.toJSONString(entry.getValue()), PromptNode.class);
+			addNode(entry.getKey(), promptNode);
+			
+			if (Integer.valueOf(entry.getKey()) > maxNodeId) {
+				maxNodeId = Integer.valueOf(entry.getKey());
+			}
+		}
+		
+		// 最大序号
+		if (idMap == null) {
+			idMap = new HashMap<>();
+		}
+		idMap.put("nodeId", maxNodeId);
+	}
+
+	public Map<String, PromptNode> getNodes() {
+		return nodes;
+	}
+
+	public boolean containsKey(String key) {
+		return getNodes().containsKey(key);
+	}
+
+	// 添加一个节点
+    public void addNode(String nodeId, PromptNode node) {
+    	node.setNodeId(nodeId);
+        nodes.put(nodeId, node);
+    }
+    
+    // 添加一个节点
+    public void addNode(PromptNode node) {
+    	String nodeId;
+    	if (node.getNodeId() == null) {
+    		nodeId = nextNodeId();
+        	node.setNodeId(nodeId);
+		} else {
+			nodeId = node.getNodeId();
+		}
+    	nodes.put(nodeId, node);
+    }
+    
+    // 追加一个新节点
+    public void appendNode(PromptNode node) {
+    	String nodeId;
+    	nodeId = nextNodeId();
+		node.setNodeId(nodeId);
+    	nodes.put(nodeId, node);
+    }
+    
+    // 删除一个节点
+    public void removeNode(String nodeId) {
+    	if (nodes.containsKey(nodeId)) {
+    		nodes.remove(nodeId);
+		}
+    }
+
+    // 生成最终的 JSON 结构
+    public Map<String, Object> toMap() {
+        Map<String, Object> promptMap = new LinkedHashMap<>();
+        for (Map.Entry<String, PromptNode> entry : nodes.entrySet()) {
+            promptMap.put(entry.getKey(), entry.getValue().toMap());
+        }
+        return promptMap;
+    }
+    
+    public String nextNodeId() {
+    	if (idMap == null) {
+			idMap = new HashMap<>();
+			idMap.put("nodeId", 1);
+		} else {
+			idMap.put("nodeId", idMap.get("nodeId")+1);
+		}
+    	
+    	return String.valueOf(idMap.get("nodeId"));
+    }
+    
+    public PromptNode getPromptNode(String classType) {
+    	return getPromptNode(classType, null);
+    }
+    
+    public PromptNode getPromptNode(String classType, String nodeId) {
+    	if (nodes != null) {
+    		for (Entry<String, PromptNode> entry : nodes.entrySet()) {
+    			if (entry.getValue().getClass_type().equals(classType) && ((nodeId!=null)?entry.getKey().equals(nodeId):true)) {
+    				//System.out.println("存在匹配的类："+classType);
+    				return entry.getValue();
+    			}
+    		}
+    	}
+    	
+    	return null;
+    }
+    
+}
---
+++ SendParam.java
@@ -0,0 +1,31 @@
+package com.fangg.bgm.api.bean.comfyui;
+
+import java.util.Map;
+
+import com.xclj.common.util.UuidUtil;
+
+public class SendParam {
+
+	private String client_id;	// 接口调用id
+	private Map<String, PromptNode> prompt;
+
+	public String getClient_id() {
+		if (client_id == null) {
+			client_id = UuidUtil.genUuid_0(32);
+		}
+		return client_id;
+	}
+
+	public void setClient_id(String client_id) {
+		this.client_id = client_id;
+	}
+
+	public Map<String, PromptNode> getPrompt() {
+		return prompt;
+	}
+
+	public void setPrompt(Map<String, PromptNode> prompt) {
+		this.prompt = prompt;
+	}
+	
+}
